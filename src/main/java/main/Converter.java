d01c848da81edfb4597ccd03d08f9982f58f9be1
Chris Mendoza <chesmend@ucsc.edu>
2021-05-17 07:53:03 -0700

    Added scanner functionality and menu

--- src/main/java/main/Converter.java
+++ src/main/java/main/Converter.java
@@ -1,10 +1,27 @@
 package main;
 
+import java.util.Scanner;
+
 public class Converter {
 	
 	public static void main(String args[]) {
-		int menuSelection, lastSelection;
+		int menuSelectionInt = 0;
+		String menuSelectionStr = "";
+		Scanner scan = new Scanner(System.in);
 		
+		while(menuSelectionInt != 4) {
+			System.out.println("Please select an option:\n"
+							 + "1. Cups to Teaspoons\n"
+							 + "2. Miles to Kilometers\n"
+							 + "3. US Gallons to Imperial Gallons\n"
+							 + "4. Quit");
+			menuSelectionStr = scan.nextLine();
+			menuSelectionInt = Integer.parseInt(menuSelectionStr);
+			
+			
+		}
+		
+		scan.close();
 	}
 
 }
